fed87391961935a4e234f85f689a2898e4c64976
derrickyct <derrickyiuct@gmail.com>
2021-09-15 03:53:57 -0400

    modified setting

--- outdoorthermal/settings.py
+++ outdoorthermal/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'static')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
